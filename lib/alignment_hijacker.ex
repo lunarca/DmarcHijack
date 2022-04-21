f37d8fc4bb743b8bcc199f18d313faf84109c3fd
Alex DeFreese <alexdefreese@gmail.com>
2022-04-21 00:50:39 -0400

    Init

---
+++ lib/alignment_hijacker.ex
@@ -0,0 +1,18 @@
+defmodule AlignmentHijacker do
+  @moduledoc """
+  Documentation for `AlignmentHijacker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AlignmentHijacker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/alignment_hijacker_test.exs
@@ -0,0 +1,8 @@
+defmodule AlignmentHijackerTest do
+  use ExUnit.Case
+  doctest AlignmentHijacker
+
+  test "greets the world" do
+    assert AlignmentHijacker.hello() == :world
+  end
+end
