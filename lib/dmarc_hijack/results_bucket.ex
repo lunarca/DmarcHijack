a838c9d581c2b1a932c293fda2b4325a41ee6743
Alex DeFreese <alexdefreese@gmail.com>
2022-04-21 20:47:27 -0400

    Add an agent to store results

---
+++ lib/dmarc_hijack/results_bucket.ex
@@ -0,0 +1,33 @@
+defmodule DmarcHijack.ResultsBucket do
+  use Agent
+
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  @doc """
+  Get the associated value from a domain
+  """
+  @spec get(String.t()) :: {atom, atom}
+  def get(domain) do
+    case Agent.get(__MODULE__, fn state -> Map.get(state, domain) end) do
+      nil -> {:error, :not_available}
+      results -> results
+    end
+  end
+
+  @doc """
+  Return the whole state map
+  """
+  def getAll() do
+    {:ok, Agent.get(__MODULE__, fn state -> state end)}
+  end
+
+  def add(domain, result, policy) do
+    Agent.update(__MODULE__, fn state -> Map.put(state, domain, {result, policy}) end)
+  end
+
+end
